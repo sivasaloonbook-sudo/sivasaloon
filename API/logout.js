e36dda773b8f998a9b7ecaa49c2e2c013a063c70
sivasaloonbook-sudo <sivasaloonbook@gmail.com>
2026-08-01 15:19:11 +0530

    Delete API directory

--- API/logout.js
+++
@@ -1,8 +0,0 @@
-const { clearSessionCookie, COOKIE_CUSTOMER, COOKIE_ADMIN } = require("./_lib/auth");
-
-module.exports = async (req, res) => {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-  clearSessionCookie(res, COOKIE_CUSTOMER);
-  clearSessionCookie(res, COOKIE_ADMIN);
-  return res.status(200).json({ ok: true });
-};
